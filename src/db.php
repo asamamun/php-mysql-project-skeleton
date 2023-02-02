70265f0d7e02ce931feb440691b63f78939f0c3f
asamamun <asamamun.web@gmail.com>
2023-02-02 18:52:20 +0600

    exception checked for db connection

--- src/db.php
+++ src/db.php
@@ -3,7 +3,14 @@ namespace App;
 class db{
     public static $conn;
     public static function connect(){
-        self::$conn = new \mysqli(settings()['hostname'],settings()['user'],settings()['password'],settings()['database']) or die("<h1>Connection ERROR!!</h1>");
+        try{
+            self::$conn = new \mysqli(settings()['hostname'],settings()['user'],settings()['password'],settings()['database']) or die("<h1>Connection ERROR!!</h1>");
         return self::$conn;
+        }
+        catch(\Exception $e){
+            echo "<h1>DB Connection ERROR!!</h1>";
+            exit;
+        }
+        
     }
 }
